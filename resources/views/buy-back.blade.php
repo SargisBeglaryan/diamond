608c95f276ee724daceb4f39e91130fbf800fb97
Sargis <sargisbelaryan1989@gmail.com>
2018-10-08 13:58:37 +0400

    Build investmend sub pages.

---
+++ resources/views/buy-back.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts/main')
+@section('title') {{'Diamond buy-back'}} @endsection
+@section('style')
+    @parent
+@endsection
+@section("content")
+<div class="page-title-content">
+	<div class="row justify-content-md-center">
+		<div class="col-md-10">
+			<h1 class="page-title">Diamond buy-back</h1>
+		</div>
+	</div>
+</div>
+<div class="content-block bg-light">
+	<div class="row justify-content-md-center">
+		<div class="col-md-10">
+			<div class="row">
+				<div class="col-md-12">
+					<div  class="information-content buy-block-content text-left">
+						<h4 class="information-title">Rückkauf & Umtausch</h4>
+						<p class="information-decription">Ein Rückkauf/Umtausch ist jederzeit möglich. Unsere Kundenberater informieren Sie gerne über die aktuellen Marktpreise, falls Sie einen Diamanten umtauschen oder verkaufen möchten.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
---
+++ resources/views/crisis-immunity.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts/main')
+@section('title') {{'Absolutely immune to crises'}} @endsection
+@section('style')
+    @parent
+@endsection
+@section("content")
+<div class="page-title-content">
+	<div class="row justify-content-md-center">
+		<div class="col-md-10">
+			<h1 class="page-title">Absolutely immune to crises</h1>
+		</div>
+	</div>
+</div>
+<div class="content-block" style="background-color: #c3c3c3">
+	<div class="row justify-content-md-center">
+		<div class="col-md-10">
+			<div class="row">
+				<div class="col-md-6">
+					<div  class="information-content crisis-block-content text-left">
+						<h4 class="information-title">Keine Entwertung</h4>
+						<p class="information-decription">Diamanten können nie entwertet werden ­ Das einzigartige an Diamanten ist, dass keine Regierung und keine Notenbank der Welt Diamanten herstellen und durch, inflationären Gebrauch, entwerten können.<br><br>
+
+						Das unterscheidet Diamanten von allen anderen auf Papier gedruckten Anlageformen wie Aktien, Anleihen, Banknoten oder Kontoguthaben. Wenn diese Papierwerte an Wert verloren haben, kann der Eigentümer von Diamanten immer noch überall bezahlen.<br><br>
+
+						Der durchschnittliche Preisanstieg zwischen 1960 und 2013 beträgt jährlich 14.47%. Diamanten sind extrem selten und ein natürliches Mineral kombiniert mit der Kunst des Schleifens bis zur Perfektion.</p>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<img class="image-block" src="{{asset('imgs/diamantpreis.png')}}" alt="Diamantpreis">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="content-block bg-light">
+	<div class="row justify-content-md-center">
+		<div class="col-md-10">
+			<div class="row">
+				<div class="col-md-12">
+					<div  class="information-content crisis-block-content text-left">
+						<h4 class="information-title">Eine sichere Investition</h4>
+						<p class="information-decription">Dadurch, dass die Qualität eines Diamanten von eines der drei weltweiten anerkannten Diamanten Labore (HRD, GIA, IGI) zertifiziert werden und dies in einem entsprechenden Zertifikat festgehalten wird, sind Diamanten klar klassifiziert und definiert.</p>
+					</div>
+				</div>
+				<div class="col-md-12">
+					<div  class="information-content crisis-block-content text-left">
+						<h4 class="information-title">Sicherheit vor Währungsreformen & Bankenkrisen</h4>
+						<p class="information-decription">Diamanten sind ein krisensicheres Investment; Wertbeständige Anlage, Inflationsschutz, kein Totalverlust, Altersvorsorge, Anonymität (weil der Diamantverkauf keiner Meldepflicht unterliegt).<br><br>
+
+						Diamanten sind das kleinste, leichteste und die konzentrierteste Form des Reichtums, die in der Welt existiert. Seit über 50 Jahren hat sich der Preis von Diamanten im Schnitt um 14.47% pro Jahr gesteigert.<br><br>
+						
+						Diamanten sind daher eine zuverlässige Wertanlage und haben eine nachgewiesene Erfolgsbilanz von stabil steigenden Preisen. Darüber hinaus sind Diamantenpreise resistent gegen finanziell volatile Zeiten und Bankenkrisen.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
